fec1598302a690959b0dc0dd9c7e44caae1191f9
ssar <ssar@example.com>
2021-10-08 14:14:04 +0900

    기본개발환경설정

---
+++ src/main/java/com/cos/navercraw/NaverCrawApplication.java
@@ -0,0 +1,13 @@
+package com.cos.navercraw;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NaverCrawApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NaverCrawApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/cos/navercraw/NaverCrawApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cos.navercraw;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NaverCrawApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

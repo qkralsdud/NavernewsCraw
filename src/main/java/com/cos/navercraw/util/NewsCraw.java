44950e85ef29565250e3a6589262676157adabe7
ssar <ssar@example.com>
2021-10-11 17:48:41 +0900

    코드 수정

--- src/main/java/com/cos/navercraw/util/NewsCraw.java
+++ src/main/java/com/cos/navercraw/util/NewsCraw.java
@@ -16,49 +16,44 @@ import com.cos.navercraw.domain.News;
 @Component
 public class NewsCraw {
 	int aidNum = 1;
-	
+
 	public List<News> collect() {
 		RestTemplate rt = new RestTemplate();
 		List<News> newsList = new ArrayList<>();
 
-		for (int i = 1; i < 6; i++) {
-			String aid = String.format("%010d", aidNum);
-			String url = "https://news.naver.com/main/read.naver?mode=LSD&mid=shm&sid1=103&oid=437&aid=" + aid;
+		String aid = String.format("%010d", aidNum);
+		String url = "https://news.naver.com/main/read.naver?mode=LSD&mid=shm&sid1=103&oid=437&aid=" + aid;
 
-			try {
-				String html = rt.getForObject(url, String.class);
+		try {
+			String html = rt.getForObject(url, String.class);
 
-				Document doc = Jsoup.parse(html);
+			Document doc = Jsoup.parse(html);
 
-				Element companyElement = doc.selectFirst("#main_content > div.article_header > div.press_logo > a > img");
-				Element titleElement = doc.selectFirst("#articleTitle");
-				Element createdAtElement = doc.selectFirst(".t11");
-				String company = companyElement.attr("alt");
-				String title = titleElement.text();
-				String cAt = createdAtElement.text();
-				
-				Timestamp tcAt =  Timestamp.valueOf(cAt);
-				LocalDateTime lcAt = tcAt.toLocalDateTime().minusDays(1).plusHours(9);
-				Timestamp createdAt = Timestamp.valueOf(lcAt);
-				
-				LocalDateTime startTime = LocalDateTime.now().minusDays(1).plusHours(9);
-				Timestamp startTs = Timestamp.valueOf(startTime);
-				
-				if(createdAt == startTs) {
-					News news = News.builder().company(company).title(title).createdAt(createdAt).build();
-					
-					newsList.add(news);					
-				} else {
-					Exception e = new Exception();
-					System.out.println("오류:" + e.getMessage());
-				}
-				
-			} catch (Exception e) {
-				System.out.println("오류:" + e.getMessage());
-			} 
+			Element companyElement = doc.selectFirst("#main_content > div.article_header > div.press_logo > a > img");
+			Element titleElement = doc.selectFirst("#articleTitle");
+			Element createdAtElement = doc.selectFirst(".t11");
+			String company = companyElement.attr("alt");
+			String title = titleElement.text();
+			String cAt = createdAtElement.text();
 
-			aidNum++;
-		} 
+			Timestamp tcAt = Timestamp.valueOf(cAt);
+			LocalDateTime lcAt = tcAt.toLocalDateTime().minusDays(1).plusHours(9);
+			Timestamp createdAt = Timestamp.valueOf(lcAt);
+
+			LocalDateTime startTime = LocalDateTime.now().minusDays(1).plusHours(9);
+			Timestamp startTs = Timestamp.valueOf(startTime);
+
+			if (createdAt == startTs) {
+				News news = News.builder().company(company).title(title).createdAt(createdAt).build();
+
+				newsList.add(news);
+			}
+
+		} catch (Exception e) {
+			System.out.println("오류:" + e.getMessage());
+		}
+
+		aidNum++;
 
 		return newsList;
 	}
